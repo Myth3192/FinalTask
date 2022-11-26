ef159e78fa55fb86a7324231769985f1418d19ab
VladimirIvanov <ivanovvladimir311992@gmail.com>
2022-11-26 20:19:15 +0300

    Comparison of two arrays

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -4,3 +4,13 @@ Console.WriteLine("Введите массив через запятую");
 string arrayenter = Console.ReadLine();
 string[] array = arrayenter.Split(",");
 string[] array2 = new string[array.Length];
+
+for (int i=0; i<array.Length; i++)
+{
+   if (array[i].Length <= 3)
+   {
+        array2[i] = array[i];
+        
+    }
+    
+}
